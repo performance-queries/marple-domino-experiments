0ab17e3200df66798df87a7f0f4e57bfc6130659
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-22 22:17:41 -0500

    Progress indicator

--- generate_results.sh
+++ generate_results.sh
@@ -2,6 +2,7 @@
 rm -rf mul_acc.txt
 rm -rf nested_ifs.txt
 for f in `ls marple/example_queries/*`; do
+  echo "Now running $f"
   cat $f | java -ea -jar marple/target/Compiler-jar-with-dependencies.jar > /dev/null 2>&1
   echo $f >> mul_acc.txt
   ./compile.sh domino-full.c mul_acc.sk    15 15 >> mul_acc.txt
