4522e009e4dddf79d857f2a25e7f3b0e5cdad31b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-22 22:23:08 -0500

    Newline should be in the output file, not console

--- generate_results.sh
+++ generate_results.sh
@@ -4,9 +4,12 @@ rm -rf nested_ifs.txt
 for f in `ls marple/example_queries/*`; do
   echo "Now running $f"
   cat $f | java -ea -jar marple/target/Compiler-jar-with-dependencies.jar > /dev/null 2>&1
+
   echo $f >> mul_acc.txt
   ./compile.sh domino-full.c mul_acc.sk    15 15 >> mul_acc.txt
+  echo -e "\n" >> mul_acc.txt
+
   echo $f >> nested_ifs.txt
   ./compile.sh domino-full.c nested_ifs.sk 15 15 >> nested_ifs.txt
-  echo -e "\n"
+  echo -e "\n" >> nested_ifs.txt
 done
