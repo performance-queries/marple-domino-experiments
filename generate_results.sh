cce25b0e5418e9989e1bb7398f942123b9a57443
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-22 22:16:44 -0500

    Bug fixes

--- generate_results.sh
+++ generate_results.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
+rm -rf mul_acc.txt
+rm -rf nested_ifs.txt
 for f in `ls marple/example_queries/*`; do
   cat $f | java -ea -jar marple/target/Compiler-jar-with-dependencies.jar > /dev/null 2>&1
-  echo $f > mul_acc.txt
-  ./compile.sh domino-full.c mul_acc.sk    15 15 > mul_acc.txt
-  echo $f > nested_ifs.txt
-  ./compile.sh domino-full.c nested_ifs.sk 15 15 > nested_ifs.txt
+  echo $f >> mul_acc.txt
+  ./compile.sh domino-full.c mul_acc.sk    15 15 >> mul_acc.txt
+  echo $f >> nested_ifs.txt
+  ./compile.sh domino-full.c nested_ifs.sk 15 15 >> nested_ifs.txt
 done
