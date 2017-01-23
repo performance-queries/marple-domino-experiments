1f65fb23f0aeb50d21b12fc8a5ba8769f5e73498
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-22 22:49:57 -0500

    Store figures as well

--- generate_results.sh
+++ generate_results.sh
@@ -8,8 +8,11 @@ for f in `ls marple/example_queries/*`; do
   echo $f >> mul_acc.txt
   ./compile.sh domino-full.c mul_acc.sk    15 15 >> mul_acc.txt
   echo -e "\n" >> mul_acc.txt
+  mv /tmp/pipeline.png `echo ${f} | cut -d '/' -f 3 | cut -d '.' -f 1`_mul_acc.png
 
   echo $f >> nested_ifs.txt
   ./compile.sh domino-full.c nested_ifs.sk 15 15 >> nested_ifs.txt
   echo -e "\n" >> nested_ifs.txt
+  mv /tmp/pipeline.png `echo ${f} | cut -d '/' -f 3 | cut -d '.' -f 1`_nested_ifs.png
+
 done
