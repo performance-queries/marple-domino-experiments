87ed6d2480edf33093829267829b25fc5ed0dcac
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-22 22:19:13 -0500

    Print out a newline after every file

--- generate_results.sh
+++ generate_results.sh
@@ -8,4 +8,5 @@ for f in `ls marple/example_queries/*`; do
   ./compile.sh domino-full.c mul_acc.sk    15 15 >> mul_acc.txt
   echo $f >> nested_ifs.txt
   ./compile.sh domino-full.c nested_ifs.sk 15 15 >> nested_ifs.txt
+  echo -e "\n"
 done
